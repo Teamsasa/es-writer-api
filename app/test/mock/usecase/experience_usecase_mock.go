34c4ac376de2c78f24bcde245b8d7e22abcf62ef
Yutosaki <sasayu8190@gmail.com>
2025-03-15 16:37:38 +0900

    feat: mockをcontextを使用するように

--- app/test/mock/usecase/experience_usecase_mock.go
+++ app/test/mock/usecase/experience_usecase_mock.go
@@ -1,18 +1,19 @@
 package mock
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/mock"
+	"context"
 
 	"es-api/app/internal/entity/model"
+
+	"github.com/stretchr/testify/mock"
 )
 
 type ExperienceUsecaseMock struct {
 	mock.Mock
 }
 
-func (m *ExperienceUsecaseMock) GetExperienceByUserID(c echo.Context) (*model.Experiences, error) {
-	args := m.Called(c)
+func (m *ExperienceUsecaseMock) GetExperienceByUserID(ctx context.Context) (*model.Experiences, error) {
+	args := m.Called(ctx)
 
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
@@ -21,8 +22,8 @@ func (m *ExperienceUsecaseMock) GetExperienceByUserID(c echo.Context) (*model.Ex
 	return args.Get(0).(*model.Experiences), args.Error(1)
 }
 
-func (m *ExperienceUsecaseMock) PostExperience(c echo.Context, inputExperience model.InputExperience) (*model.Experiences, error) {
-	args := m.Called(c, inputExperience)
+func (m *ExperienceUsecaseMock) PostExperience(ctx context.Context, inputExperience model.InputExperience) (*model.Experiences, error) {
+	args := m.Called(ctx, inputExperience)
 
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
