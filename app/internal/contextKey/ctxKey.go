c1ae245c60de6aab777557a39f9490d5cea1febf
Yutosaki <sasayu8190@gmail.com>
2025-03-18 17:35:40 +0900

    feat: 型を新規に定義

---
+++ app/internal/contextKey/ctxKey.go
@@ -0,0 +1,10 @@
+package contextKey
+
+type userIDKey struct {}
+var UserIDKey = userIDKey{}
+
+type idpKey struct {}
+var IDPKey = idpKey{}
+
+type keywordKey struct {}
+var KeywordKey = keywordKey{}
